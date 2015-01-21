f7770491bc300a169ce1e3e3ec8569df743afc90
Rizki Sunaryo <rizkisunaryo@gmail.com>
2015-01-21 18:13:55 +0800

    - binding methods to TAGManager

--- ios/src/org/robovm/bindings/googleanalytics/TAGManager.java
+++ ios/src/org/robovm/bindings/googleanalytics/TAGManager.java
@@ -1,6 +1,7 @@
 package org.robovm.bindings.googleanalytics;
 
 import org.robovm.apple.foundation.NSObject;
+import org.robovm.objc.annotation.Method;
 import org.robovm.objc.annotation.NativeClass;
 
 /**
@@ -9,4 +10,31 @@ import org.robovm.objc.annotation.NativeClass;
 
 @NativeClass
 public class TAGManager extends NSObject {
+//    + (GAI *)sharedInstance;
+    @Method(selector = "sharedInstance:")
+    public static native void sharedInstance(GAI request);
+
+//    - (id<GAITracker>)trackerWithName:(NSString *)name
+    @Method(selector = "trackerWithName:")
+    public native GAITracker trackerWithName(String name);
+
+//    trackingId:(NSString *)trackingId;
+    @Method(selector = "trackingId:")
+    public native void trackingId(String trackingId);
+
+//    - (id<GAITracker>)trackerWithTrackingId:(NSString *)trackingId;
+    @Method(selector = "trackerWithTrackingId:")
+    public native GAITracker trackerWithTrackingId(String trackingId);
+
+//    - (void)removeTrackerByName:(NSString *)name;
+    @Method(selector = "removeTrackerByName:")
+    public native void removeTrackerByName(String name);
+
+//    - (void)dispatch;
+    @Method(selector = "dispatch:")
+    public native void dispatch();
+
+//    - (void)dispatchWithCompletionHandler:(void (^)(GAIDispatchResult))completionHandler;
+    @Method(selector = "dispatchWithCompletionHandler:")
+    public native void dispatchWithCompletionHandler();
 }
