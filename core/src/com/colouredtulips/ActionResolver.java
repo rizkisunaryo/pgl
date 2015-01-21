c6010934743bd533861170f05bf95fb9ca6de519
Rizki Sunaryo <rizkisunaryo@gmail.com>
2015-01-21 11:53:09 +0800

    add Google Analytics but error

---
+++ core/src/com/colouredtulips/ActionResolver.java
@@ -0,0 +1,10 @@
+package com.colouredtulips;
+
+/**
+ * Created by rizkisunaryo on 1/21/15.
+ */
+public interface ActionResolver {
+    // ...
+    public void setTrackerScreenName(String path);
+
+}
